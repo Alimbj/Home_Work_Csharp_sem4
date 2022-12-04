2563bdf9cd42bbe95807a2248153dfc082432720
Alim <bj09@mail.ru>
2022-12-04 19:34:51 +0300

    Add

--- Program.cs
+++ Program.cs
@@ -35,111 +35,74 @@ foreach (var element in array)
 // или так Console.WriteLine($"[{string.Join(", ", array)}]");
 */
 
-//Доплнительная задача
 
-Console.Write("Введите число "); // Вводим числа и создаем список
-int N = int.Parse(Console.ReadLine());
-int[] array_first = new int[N];
-for (int i = 0; i < N; i++){
-    int q = int.Parse(Console.ReadLine());
-    array_first[i] = q;
-}
-//Console.WriteLine($"[{string.Join(", ", array_first)}]");
-Console.WriteLine("Введите второе чилсо ");
-int K = int.Parse(Console.ReadLine());  
-int[] array_second = new int[N]; //Создаем второй список
-if (K > 1 && K < N){  // Заполняем второй список элементами первого для случая когда 0 < K < N
-    int w = K - 1;
-    for (int j = 0; j < K; j++){  // отрезок нового списка от 0 до K-1
-        if (w == N)
-            break;
-        array_second[j] = array_first[w];
-        w++;
-    }
-    int e = 0;
-    for (int o = N - K; o < N; o++){ //Отрезок нового списка от K до N-1
-        array_second[o] = array_first[e];
-        e++;
-    }
-}
-else if (K > N){  // Заполняем второй список элементами первого для случая когда  K > N
-    int w = K;
-    while (w > N)
-        w = K - N;
-    int r = w;
-    int count1 = 0;
-    w--;
-    for (int p = 0; p < N; p++){
-        array_second[p] = array_first[w];
-        w++;
-        count1++; 
-        if (w >= N)
-            break;
-        Console.WriteLine(w);
-    }
-    int e = 0;
-    for (int j = count1; j < N; j++){
-        array_second[j] = array_first[e];
-        e++;
-    }
+//Доп. решение доп задачки
 
-}
-// Далее рассматриваем случаи когда K отрицательное
-else if (K < -1){
-    if (-K < N){ // Приводим K к положительному(-K) значению и сравниваем с N, заполняем новый список для случая когда -K < N
-        int u = -K;
-        for (int t = 0; t < N-K; t++){
-            array_second[t] = array_first[u];
-            u++;
-        }
-        int p = 0;
-        for (int y = -K; y < N; y++){
-            array_second[p] = array_first[N-u];
-            p++;
-        }
-    }
-    else if (-K > N){  // Делаем то же самое что выше и заполняем новый список, но для случая когда -K > N
-        int s = -K;
-        while (s >= N)
-            s = -K - N;
-        int r = s;
-        for (int p = 0; p < r; p++){
-            array_second[s] = array_first[p];
-            s++;
-        }
-        int e = 0;
-        for (int i = r; i < N; i++){
-            array_second[i] = array_first[e];
-            e++;
-        }
-    }
-}
-else
-    array_second = array_first;
-Console.WriteLine($"[{string.Join(", ", array_first)}]");
-Console.WriteLine($"[{string.Join(", ", array_second)}]");
-
-
-//Другое решение доп задачки
 /*
+
 Console.WriteLine("Введите первое число");
 int Nu = int.Parse(Console.ReadLine());
 int[] array_third = new int[Nu];
+int[] array_fourth = new int[Nu];
 for (int i = 0; i < Nu; i++){
     int qq = int.Parse(Console.ReadLine());
     array_third[i] = qq;
 }
 Console.WriteLine("Введите второе число");
 int Ch = int.Parse(Console.ReadLine());
-int[] array_fourth = new int[Nu];
-int temp = 0;
-while (temp != Ch){
-    array_fourth[temp] = array_third[Ch-1];
-    temp++;
-    Ch++;
-    if (Ch > Nu)
-        break;
+int temp1 = 0;
+int temp2 = 0;
+if (Ch > 0){
+    if (Ch < Nu){
+        for (int i = Nu - Ch; i < Nu; i++){
+            array_fourth[temp1] = array_third[i];
+            temp1++;
+        }
+        for (int j = Ch; j < Nu; j++){
+            array_fourth[j] = array_third[temp2];
+            temp2++;
+        }
+    }
+    if (Ch > Nu){
+        while (Ch > Nu)
+            Ch = Ch - Nu;
+        Console.WriteLine(Ch);
+        for (int i = Nu - Ch; i < Nu; i++){
+            array_fourth[temp1] = array_third[i];
+            temp1++;
+        }
+        for (int j = Ch; j < Nu; j++){
+            array_fourth[j] = array_third[temp2];
+            temp2++;
+        }
+    }
 }
+if (Ch < 0){
+    if (-Ch < Nu){
+        for (int i = Nu + Ch; i < Nu; i++){
+            array_fourth[i] = array_third[temp1];
+            temp1++;
+        }
+        for (int j = -Ch; j < Nu; j++){
+            array_fourth[temp2] = array_third[j];
+            temp2++;
+        }
+    }
+    if (-Ch > Nu){
+        while (-Ch > Nu)
+            Ch = Nu + Ch;
+        Console.WriteLine(Ch);
+        for (int i = Nu + Ch; i < Nu; i++){
+            array_fourth[i] = array_third[temp1];
+            temp1++;
+        }
+        for (int j = -Ch; j < Nu; j++){
+            array_fourth[temp2] = array_third[j];
+            temp2++;
+        }
+    }
+}
+
 Console.WriteLine($"[{string.Join(", ", array_third)}]");
 Console.WriteLine($"[{string.Join(", ", array_fourth)}]");
 */
